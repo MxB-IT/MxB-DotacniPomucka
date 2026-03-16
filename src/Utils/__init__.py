e38fd3d5c4f09466b85afebed10b7a6ab45ec0f9
Lander456 <tedus.lp@gmail.com>
2026-03-16 21:46:53 +0100

    [FIX] Deleted unneeded SeparatorGetter class, since using Path should handle everything related to file separators just fine

--- src/Utils/__init__.py
+++ src/Utils/__init__.py
@@ -1,6 +1,5 @@
 from src.Utils.error_handler import ErrorHandler
 from src.Utils.resource_finder import ResourceFinder
-from src.Utils.separator_getter import SeparatorGetter
 from src.Utils.success_handler import SuccessHandler
 
-__all__ = ["ErrorHandler", "ResourceFinder", "SeparatorGetter", "SuccessHandler"]
+__all__ = ["ErrorHandler", "ResourceFinder", "SuccessHandler"]
