d754426705df58a108f51886f3f3f0177244b1fa
Lander456 <tedus.lp@gmail.com>
2026-04-23 16:28:54 +0200

    [FEATURE] Added a mapper since the program now needs to ascertain the insurance company code from their names

---
+++ src/Mappers/insurance_company_mapper.py
@@ -0,0 +1,28 @@
+"""Defines the mappings for translating insurance company names to their numbers"""
+from types import MappingProxyType
+
+
+class InsuranceCompanyMapper:
+    """Defines the mappings for translating insurance company names to their numbers"""
+    _MAPPING: MappingProxyType[str, int] = MappingProxyType({
+        "Všeobecná zdravotní pojišťovna ČR": 111,
+        "Vojenská zdravotní pojišťovna ČR": 201,
+        "Česká průmyslová zdravotní pojišťovna": 205,
+        "Oborová zdravotní pojišťovna zaměstnanců bank, pojišťoven a stavebnictví": 207,
+        "Zaměstnanecká pojišťovna Škoda": 209,
+        "Zdravotní pojišťovna ministerstva vnitra ČR": 211,
+        "RBP, zdravotní pojišťovna": 213
+    })
+
+    @classmethod
+    def from_str(cls, value: str) -> int:
+        """
+        Converts the given string to an insurance company number
+        :param value: string to convert to an insurance company number
+        :return: int representing the company number
+        :raises ValueError: if an invalid string is passed
+        """
+        try:
+            return cls._MAPPING[value]
+        except KeyError as e:
+            raise ValueError(f"Invalid insurance company name: {value}") from e
